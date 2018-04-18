e6bf08de985aef742accc70d802474dcbc061942
Synodiporos <Synodiporos@DESKTOP-SGP3UBB>
2018-04-19 00:30:14 +0300

    Test commit

--- UV_Lamp_Controller/sloeber.ino.cpp
+++ UV_Lamp_Controller/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-04-18 22:57:19
+//This file has been generated on 2018-04-19 00:29:54
 
 #include "Arduino.h"
 #include "Arduino.h"
