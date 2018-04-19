50dca923933d2f02b2880c66a17c326c06df4bdc
sgeorgiadis <sgeorgiadis@RAY-MB311.raycap.gr>
2018-04-19 16:52:59 +0300

    Coordinate Clas created

---
+++ UV_Lamp_Controller/Geometry/Coordinates.cpp
@@ -0,0 +1,18 @@
+/*
+ * Coordinates.cpp
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: sgeorgiadis
+ */
+
+#include "Coordinates.h"
+
+Coordinates::Coordinates() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Coordinates::~Coordinates() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ UV_Lamp_Controller/Geometry/Coordinates.h
@@ -0,0 +1,17 @@
+/*
+ * Coordinates.h
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: sgeorgiadis
+ */
+
+#ifndef GEOMETRY_COORDINATES_H_
+#define GEOMETRY_COORDINATES_H_
+
+class Coordinates {
+public:
+	Coordinates();
+	virtual ~Coordinates();
+};
+
+#endif /* GEOMETRY_COORDINATES_H_ */
