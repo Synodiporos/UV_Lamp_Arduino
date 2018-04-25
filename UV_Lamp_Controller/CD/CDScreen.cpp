ed8535dbb60a348ec8e31eb68c577ee021f783e2
sgeorgiadis <sgeorgiadis@RAY-MB311.raycap.gr>
2018-04-25 09:46:49 +0300

    CD started classes

---
+++ UV_Lamp_Controller/CD/CDScreen.cpp
@@ -0,0 +1,18 @@
+/*
+ * CDScreen.cpp
+ *
+ *  Created on: Apr 24, 2018
+ *      Author: sgeorgiadis
+ */
+
+#include "CDScreen.h"
+
+CDScreen::CDScreen() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CDScreen::~CDScreen() {
+	// TODO Auto-generated destructor stub
+}
+
